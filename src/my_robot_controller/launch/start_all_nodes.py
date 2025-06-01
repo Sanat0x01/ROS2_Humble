48f35fb4839f8bc00bd138f6288426de505a3634
Sanat <ec24b1069@iiitdm.ac.in>
2025-06-01 14:14:37 +0530

    Update start_all_nodes.py launch file

--- src/my_robot_controller/launch/start_all_nodes.py
+++ src/my_robot_controller/launch/start_all_nodes.py
@@ -3,24 +3,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(
-            package='my_robot_controller',
-            executable='test_node',
-            name='test_node',
-            output='screen'
-        ),
-        Node(
-            package='my_robot_controller',
-            executable='draw_circle',
-            name='draw_circle',
-            output='screen'
-        ),
-        Node(
-            package='my_robot_controller',
-            executable='pose_subscriber',
-            name='pose_subscriber',
-            output='screen'
-        ),
+        
         Node(
             package='my_robot_controller',
             executable='number_publisher',
